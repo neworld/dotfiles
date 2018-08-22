f63c9096d5824f3684f903ee7a6b7febbc4eda9a
neworld <aneworld@gmail.com>
2018-08-22 22:03:18 +0300

    Small improvements

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -317,9 +317,9 @@ screens = [
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(),
          start=lazy.window.get_position()),
-    Drag([mod], "Button2", lazy.window.set_size_floating(),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
          start=lazy.window.get_size()),
-    Click([mod], "Button3", lazy.window.bring_to_front())
+    Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
