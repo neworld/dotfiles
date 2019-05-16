7bb40a07936365456b00ed97701cf95e26bd1031
neworld <aneworld@gmail.com>
2019-05-16 20:11:58 +0300

    2019-05-16

--- bin/screen_layouts/laptop_only.sh
+++ bin/screen_layouts/laptop_only.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-xrandr --output eDP1 --mode 3840x2160 --output DP3 --off
+xrandr --output eDP1 --mode 3840x2160 --output DP3 --off --output DP1 --off --output DP2 --off
 
--- bin/screen_layouts/two_screens_4k.sh
+++ bin/screen_layouts/two_screens_4k.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xrandr --output eDP1 --mode 3840x2160 --output DP1 --scale 1.25x1.25 --preferred --right-of eDP1
+xrandr --output eDP1 --mode 3840x2160 --output DP1 --scale 1.33x1.33 --preferred --right-of eDP1
 
 
