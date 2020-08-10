5d1d938623bea738a1fe25a8f95ec26f15eb9734
neworld <aneworld@gmail.com>
2020-08-10 18:00:23 +0300

    Improve qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -245,8 +245,8 @@ keys = [
     Key([mod], "grave", toggle_scrachpad_on_main("term")),
 
     Key([mod], "BackSpace", lazy.next_screen()),
-    Key([mod, shift], "BackSpace", switch_groups_between_screens()),
-    Key([mod, ctrl], "BackSpace", window_to_next_screen()),
+    Key([mod, ctrl], "BackSpace", switch_groups_between_screens()),
+    Key([mod, shift], "BackSpace", window_to_next_screen()),
 
     Key([], 'XF86AudioRaiseVolume', lazy.spawn(Commands.volume_up)),
     Key([], 'XF86AudioLowerVolume', lazy.spawn(Commands.volume_down)),
