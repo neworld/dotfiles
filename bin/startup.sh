f96d49ce10fe1b6251ccbeb53614082b509afcd1
neworld <aneworld@gmail.com>
2025-05-28 14:34:59 +0300

    2025-05-28

--- bin/startup.sh
+++ bin/startup.sh
@@ -6,13 +6,16 @@ setxkbmap -option terminate:ctrl_alt_bksp
 #xset r rate 200 30
 
 nm-applet &
-#pasystray &
+pasystray &
 solaar -w hide &
 #imwheel -b 45 -k
 udiskie --tray &
 lxsession &
 /opt/Synergy/synergy &
-optimus-manager-qt &
-screendimmer &
+#optimus-manager-qt &
+#iscreendimmer &
 /usr/bin/numlockx on 
+vdu_controls --system-tray --no-splash &
+/usr/bin/dunst &
+
 
